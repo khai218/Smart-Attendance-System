855f9e856d83be905392713fd893c686bfc2cb55
Albert Zack <khairulazhar2004@gmail.com>
2024-08-18 00:56:15 +0800

    1.0
    add some background and content

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,11 +8,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="kotak">
+                    <!--{{ __("You're logged in!") }} -->
+                     <br>
+                    <div class="image flex justify-center p-4"> <!-- Added padding here -->
+                        <img src="{{ asset('background/abstrak.jpg') }}" alt="Gunung Serapi" style="height: 170px; width: 1100px;">
+                    </div>
+                    <br>
+                    <h1 class="p-6 text-gray-900 font-bold">Introduction</h1>
+                    <p class="p-6 text-gray-900">The "Internet of Things" (IoT) connects everyday objects to the internet, improving efficiency across many industries. In education, IoT can enhance attendance tracking, which is often done manually and is prone to errors and dishonesty. The JTMK Smart Attendance System aims to solve these issues by using IoT technology like fingerprint scanning and facial recognition. This system will automatically record attendance, update it in real-time, and send alerts if a student's attendance falls below 80%. By replacing manual methods, this solution makes attendance tracking more accurate, secure, and efficient.</p>
                 </div>
             </div>
         </div>
     </div>
-
 </x-app-layout>
+
